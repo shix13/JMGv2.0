7b2555c8cc7c258ec6ac6640739b2efc9729cac8
geromejangj <geromeldolorfino@su.edu.ph>
2022-11-15 00:26:47 +0800

    nov15_1226am

--- JMGv2.0/form1.vb
+++ JMGv2.0/form1.vb
@@ -15,10 +15,6 @@
 
     End Sub
 
-    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
-
-    End Sub
-
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
 
     End Sub
