a602861b87f961383dce707a76ae31fcca472147
geromejangj <geromeldolorfino@su.edu.ph>
2022-11-28 10:37:03 +0800

    Nov28wPay

--- JMGv2.0/My Project/Application.Designer.vb
+++ JMGv2.0/My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.JMGv2._0.Order
+            Me.MainForm = Global.JMGv2._0.Pay
         End Sub
     End Class
 End Namespace
